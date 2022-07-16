c33539c3338885cd3e061e11262d6c6fa04687c9
Nikolaus Spring <nikolaus.spring@gmail.com>
2022-07-17 00:21:47 +0200

    Add Agent NetworkLayerGrpcClient

---
+++ src/Agent.Core/Clients/NetworkLayerGrpcClient.cs
@@ -0,0 +1,41 @@
+﻿using Agent.Core.Models;
+using Common.Grpc;
+using NetworkLayer.Grpc.Topology;
+
+namespace Agent.Core.Clients;
+
+public class NetworkLayerGrpcClient : CachedGrpcClient
+{
+    public async Task ScaleUp(Uri uri, IList<NetworkObjectCreateInfo> newNOs)
+    {
+        var channel = GetChannel(uri);
+        var client = new NetworkTopologyUpdater.NetworkTopologyUpdaterClient(channel);
+
+        var request = new ScaleUpRequest
+        {
+            NewNetworkObjects = { newNOs.Select(MapNewNetworkObject) }
+        };
+        
+        await client.ScaleUpAsync(request);
+    }
+
+    public async Task ScaleDown(Uri uri, IList<int> removeIds)
+    {
+        var channel = GetChannel(uri);
+        var client = new NetworkTopologyUpdater.NetworkTopologyUpdaterClient(channel);
+
+        var request = new ScaleDownRequest { RemoveIds = { removeIds } };
+
+        await client.ScaleDownAsync(request);
+    }
+
+    private static NewNetworkObject MapNewNetworkObject(NetworkObjectCreateInfo info)
+    {
+        return new NewNetworkObject
+        {
+            Id = info.Id,
+            Application = info.Application,
+            Groups = { info.Groups }
+        };
+    }
+}
---
+++ src/Agent.Core/Models/NetworkObjectCreateInfo.cs
@@ -0,0 +1,6 @@
+﻿namespace Agent.Core.Models;
+
+public record NetworkObjectCreateInfo(int Id, string Application)
+{
+    public List<string> Groups = new();
+}
