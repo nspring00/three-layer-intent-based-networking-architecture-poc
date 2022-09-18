16a11b1648ad5085ff783eb4b93c251c4cb15d6e
Nikolaus Spring <nikolaus.spring@gmail.com>
2022-09-18 15:16:43 +0200

    Use SQSEvent in lambda function

--- src/Agent.Lambda/src/Agent.Lambda/Function.cs
+++ src/Agent.Lambda/src/Agent.Lambda/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using Amazon.Lambda.SQSEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,12 +11,33 @@ public class Function
     /// <summary>
     /// A simple function that takes a string and returns both the upper and lower case version of the string.
     /// </summary>
-    /// <param name="input"></param>
+    /// <param name="sqsEvent"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public Casing FunctionHandler(string input, ILambdaContext context)
+    public List<Casing> FunctionHandler(SQSEvent sqsEvent, ILambdaContext context)
     {
-        return new Casing(input.ToLower(), input.ToUpper());
+        var logger = context.Logger;
+        
+        logger.LogInformation($"Beginning to process {sqsEvent.Records.Count} records...");
+
+        var casings = new List<Casing>();
+        foreach (var record in sqsEvent.Records)
+        {
+            if (record is null) continue;
+            
+            logger.LogInformation($"Message ID: {record.MessageId}");
+            logger.LogInformation($"Event Source: {record.EventSource}");
+
+            logger.LogInformation("Record Body:");
+            logger.LogInformation(record.Body);
+            
+            var casing = new Casing(record.Body.ToLower(), record.Body.ToUpper());
+            casings.Add(casing);
+        }
+
+        logger.LogInformation("Processing complete");
+        
+        return casings;
     }
 }
 
