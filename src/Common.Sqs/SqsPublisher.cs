e0cc0ffd569f292c7f16bfefdf7e4e8872419566
Nikolaus Spring <nikolaus.spring@gmail.com>
2022-09-22 22:21:22 +0200

    Add more logging to SqsPublisher

--- src/Common.Sqs/SqsPublisher.cs
+++ src/Common.Sqs/SqsPublisher.cs
@@ -20,7 +20,12 @@ public class SqsPublisher
     public async Task PublishAsync<TMessage>(string queueName, TMessage message)
         where TMessage : IMessage
     {
+        _logger.LogInformation("Publishing message to queue {QueueName}", queueName);
+
         var queueUrl = await _sqs.GetQueueUrlAsync(queueName);
+
+        _logger.LogInformation("Queue URL is {QueueUrl}", queueUrl.QueueUrl);
+
         var request = new SendMessageRequest
         {
             QueueUrl = queueUrl.QueueUrl,
@@ -28,23 +33,26 @@ public class SqsPublisher
             MessageAttributes = new Dictionary<string, MessageAttributeValue>
             {
                 {
-                    nameof(IMessage.MessageTypeName), new MessageAttributeValue
-                    {
-                        StringValue = message.MessageTypeName,
-                        DataType = "String"
-                    }
+                    nameof(IMessage.MessageTypeName),
+                    new MessageAttributeValue { StringValue = message.MessageTypeName, DataType = "String" }
                 }
             }
         };
-        await _sqs.SendMessageAsync(request);
-        
-        _logger.LogInformation("Published message to queue {QueueName}", queueName);
+        var response = await _sqs.SendMessageAsync(request);
+
+        _logger.LogInformation("Published message {ResponseId} to queue {QueueName}: {ResponseCode}",
+            response.MessageId, queueName, response.HttpStatusCode);
     }
 
     public async Task PublishAsync<TMessage>(string queueName, ICollection<TMessage> messages)
         where TMessage : IMessage
     {
+        _logger.LogInformation("Publishing {MessageCount} messages to queue {QueueName}", messages.Count, queueName);
+
         var queueUrl = await _sqs.GetQueueUrlAsync(queueName);
+
+        _logger.LogInformation("Queue URL is {QueueUrl}", queueUrl.QueueUrl);
+
         var request = new SendMessageBatchRequest
         {
             QueueUrl = queueUrl.QueueUrl,
@@ -55,19 +63,22 @@ public class SqsPublisher
                 MessageAttributes = new Dictionary<string, MessageAttributeValue>
                 {
                     {
-                        nameof(IMessage.MessageTypeName), new MessageAttributeValue
-                        {
-                            StringValue = message.MessageTypeName,
-                            DataType = "String"
-                        }
+                        nameof(IMessage.MessageTypeName),
+                        new MessageAttributeValue { StringValue = message.MessageTypeName, DataType = "String" }
                     }
                 }
             }).ToList()
         };
-        
-        await _sqs.SendMessageBatchAsync(request);
-        
+
+        var response = await _sqs.SendMessageBatchAsync(request);
+
+        response.Failed.ForEach(failure =>
+        {
+            _logger.LogError(
+                "Failed to publish message {MessageId} to queue {QueueName}: {FailureCode} {FailureSenderFault} {FailureMessage}",
+                failure.Id, queueName, failure.Code, failure.SenderFault, failure.Message);
+        });
+
         _logger.LogInformation("Published {MessageCount} messages to queue {QueueName}", messages.Count, queueName);
     }
-
 }
