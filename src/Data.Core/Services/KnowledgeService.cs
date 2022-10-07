1617a0b3ba21da3bf2dcdb05f277f3d537a680f9
Nikolaus Spring <nikolaus.spring@gmail.com>
2022-10-07 19:57:50 +0200

    Add exception handling

--- src/Data.Core/Services/KnowledgeService.cs
+++ src/Data.Core/Services/KnowledgeService.cs
@@ -2,24 +2,37 @@
 using Data.Core.Clients;
 using Data.Core.Models;
 using Data.Core.Options;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Data.Core.Services;
 
 public class KnowledgeService : IKnowledgeService
 {
+    private readonly ILogger<KnowledgeService> _logger;
     private readonly KnowledgeGrpcClient _knowledgeGrpcClient;
 
     private readonly Uri _knowledgeUri;
 
-    public KnowledgeService(KnowledgeGrpcClient knowledgeGrpcClient, IOptions<ExternalServiceConfig> externalServiceConfig)
+    public KnowledgeService(
+        ILogger<KnowledgeService> logger,
+        KnowledgeGrpcClient knowledgeGrpcClient, 
+        IOptions<ExternalServiceConfig> externalServiceConfig)
     {
+        _logger = logger;
         _knowledgeGrpcClient = knowledgeGrpcClient;
         _knowledgeUri = new Uri(externalServiceConfig.Value.KnowledgeServiceUri);
     }
     
-    public Task UpdateKnowledgeNOs(DateTime timestamp, IDictionary<Region, NetworkUpdate> updates)
+    public async Task UpdateKnowledgeNOs(DateTime timestamp, IDictionary<Region, NetworkUpdate> updates)
     {
-        return _knowledgeGrpcClient.UpdateKnowledge(_knowledgeUri, timestamp, updates);
+        try
+        {
+            await _knowledgeGrpcClient.UpdateKnowledge(_knowledgeUri, timestamp, updates);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to update knowledge");
+        }
     }
 }
